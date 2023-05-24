df3babaa1bb40b9b011f39a49a999db16ca2bb60
Cody Doucette <doucette@bu.edu>
2023-05-23 22:14:56 -0400

    Add solution

--- twostackqueue.py
+++ twostackqueue.py
@@ -7,16 +7,47 @@ class TwoStackQueue:
         self.max_length = max_length
 
     def enqueue(self, item):
-        pass
+        if self.is_full():
+            return False
+
+        self.stack1.append(item)
+        self.num_items += 1
+        return True
 
     def dequeue(self):
-        pass
+        if self.is_empty():
+            return None
+
+        for i in range(len(self.stack1) - 1):
+            self.stack2.append(self.stack1.pop())
+
+        removed = self.stack1.pop()
+
+        for i in range(len(self.stack2)):
+            self.stack1.append(self.stack2.pop())
+
+        self.num_items -= 1
+        return removed
 
     def front(self):
-        pass
+        if self.is_empty():
+            return None
+
+        for i in range(len(self.stack1) - 1):
+            self.stack2.append(self.stack1.pop())
+
+        removed = self.stack1.pop()
+
+        # make sure to put the front element back
+        self.stack1.append(removed)
+
+        for i in range(len(self.stack2)):
+            self.stack1.append(self.stack2.pop())
+
+        return removed
 
     def is_empty(self):
-        pass
+        return self.num_items == 0
 
     def is_full(self):
-        pass
+        return self.num_items == self.max_length
